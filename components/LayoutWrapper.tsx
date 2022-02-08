44d7bcf3d6e5e283d3da1d220d6fff6e1419a1d0
Naina <nainarazz@gmail.com>
2022-02-08 06:45:08 +0300

    add more styling to navbar

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -5,19 +5,21 @@ import SectionContainer from './SectionContainer';
 import Footer from './Footer';
 import ThemeSwitch from './ThemeSwitch';
 import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
 
 interface Props {
   children: ReactNode;
 }
 
 const LayoutWrapper = ({ children }: Props) => {
+  const router = useRouter();
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
         <header className="flex flex-col items-center justify-between py-10 md:flex-row">
           <div className="mb-4 md:mb-0">
             <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between decoration-blue-700 hover:underline hover:decoration-wavy">
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
                 ) : (
@@ -32,7 +34,11 @@ const LayoutWrapper = ({ children }: Props) => {
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-4 font-medium text-gray-900 dark:text-gray-100"
+                  className={`p-4 font-medium text-gray-900 dark:text-gray-100 ${
+                    router.asPath.includes(link.href)
+                      ? 'underline decoration-blue-700 decoration-wavy underline-offset-4'
+                      : ''
+                  }`}
                 >
                   {link.title}
                 </Link>
