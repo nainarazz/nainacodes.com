f13c960473799895fe0b12dbe8d696b107b03ca8
Naina <nainarazz@gmail.com>
2022-02-09 05:43:35 +0300

    add snippet nav menu

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
+  { href: '/snippets', title: 'Snippets' },
   { href: '/contact', title: 'Contact' },
 ];
 
