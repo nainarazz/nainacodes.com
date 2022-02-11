a1d67192e86bd52585eea4c0f8c5b3940adf6d9b
Naina <nainarazz@gmail.com>
2022-02-11 06:29:49 +0300

    set content in snippet page to under construction

--- pages/snippets.tsx
+++ pages/snippets.tsx
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata';
 import { PageSEO } from '@/components/SEO';
+import PageTitle from '@/components/PageTitle';
 
 export default function Snippets() {
   return (
@@ -8,6 +9,14 @@ export default function Snippets() {
       <h1 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
         Code Snippets
       </h1>
+      <div className="mt-24 text-center">
+        <PageTitle>
+          Under Construction{' '}
+          <span role="img" aria-label="roadwork sign">
+            🚧
+          </span>
+        </PageTitle>
+      </div>
     </>
   );
 }
