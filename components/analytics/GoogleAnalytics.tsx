9329a96ccae2d76bd598e42be9c43f6ff4ec92b1
Naina <nainarazz@gmail.com>
2022-02-13 19:39:47 +0300

    record pageview for google analytics on each page change

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -25,12 +25,3 @@ const GAScript = () => {
 };
 
 export default GAScript;
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const logEvent = (action, category, label, value) => {
-  window.gtag?.('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
-};
