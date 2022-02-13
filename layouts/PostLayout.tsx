7420bfcef9c82fe1c3f7a7c88bbe027e515a9db2
Naina <nainarazz@gmail.com>
2022-02-13 17:18:47 +0300

    fix markup in postlayout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -60,17 +60,16 @@ export default function PostLayout({
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10"></dl>
               <div>
                 <PageTitle>{title}</PageTitle>
                 <div className="flex flex-row justify-center">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <div>
+                    <div className="sr-only">Published on</div>
+                    <div className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                       <time dateTime={date}>
                         {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
                       </time>
-                    </dd>
+                    </div>
                     {tags && (
                       <div>
                         {tags.map((tag) => (
@@ -78,7 +77,7 @@ export default function PostLayout({
                         ))}
                       </div>
                     )}
-                  </dl>
+                  </div>
                 </div>
               </div>
             </div>
