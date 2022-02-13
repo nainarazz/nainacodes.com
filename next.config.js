16503cf1129aea0467e320e53575c113a4470de5
Naina <nainarazz@gmail.com>
2022-02-13 16:34:20 +0300

    add google tag manager to content security policy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.spotify.com utteranc.es/client.js;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.spotify.com utteranc.es/client.js https://www.googletagmanager.com/gtag/js?id=UA-164650982-1;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
