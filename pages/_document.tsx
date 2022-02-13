7d08ab8d4a553b6fb37c5cc58a69c1d2b0c557bb
Naina <nainarazz@gmail.com>
2022-02-13 16:18:26 +0300

    remove comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  // static async getInitialProps(ctx: DocumentContext) {
-  //   const initialProps = await Document.getInitialProps(ctx)
-
-  //   return initialProps
-  // }
-
   render() {
     return (
       <Html lang="en" className="scroll-smooth">
