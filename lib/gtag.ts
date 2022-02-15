7c81b53c303ae54aeb7aefdf8af4c71eb517ac90
Naina <nainarazz@gmail.com>
2022-02-15 05:43:42 +0300

    only execute page view function for google analytics in production

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,10 +1,14 @@
 import siteMetadata from '@/data/site-metadata';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag('config', siteMetadata.analytics.googleAnalyticsId, {
-    page_path: url,
-  });
+  if (isProduction) {
+    window.gtag('config', siteMetadata.analytics.googleAnalyticsId, {
+      page_path: url,
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
