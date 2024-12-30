d6b2b3c35e17537ad4c919da7ffd5459b0630cd0
Naina Razafindrabiby <nainarazz@gmail.com>
2024-12-30 20:38:22 +0300

    fix seo on projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,5 @@
 import projectsData from '@/data/projects-data';
 import Card from '@/components/Card';
-import { PageSEO } from '@/components/SEO';
 import siteMetadata from '@/data/site-metadata';
 import { Metadata } from 'next';
 
@@ -29,7 +28,6 @@ export const metadata: Metadata = {
 export default function Projects() {
   return (
     <>
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
